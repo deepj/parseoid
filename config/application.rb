8a92db002cde509e348c0f0b491af998af40c82d
deepj <deepjungle.maca@gmail.com>
2024-03-28 19:38:07 +0100

    fix: enable active_job/railtie temporarily

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,7 @@ require_relative "boot"
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie" # TODO: Enabled temporarily due to https://github.com/hotwired/turbo-rails/issues/478
 # require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
@@ -11,7 +11,7 @@ require "action_controller/railtie"
 # require "action_mailbox/engine"
 # require "action_text/engine"
 require "action_view/railtie"
-require "action_cable/engine" # Enabled temporarily due https://github.com/hotwired/turbo-rails/pull/601
+require "action_cable/engine" # TODO: Enabled temporarily due to https://github.com/hotwired/turbo-rails/issues/512
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
