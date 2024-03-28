86f994e517400d6d4cdb2f7566f9c5a64aa7e534
deepj <deepjungle.maca@gmail.com>
2024-03-28 21:09:42 +0100

    chore: clean up system tests

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -7,7 +7,7 @@ class ParserController < ApplicationController
   def preview
     @content = params[:content]
     @date = Date.parse(params[:date]) rescue nil
-puts "Content: #{@content}"
+
     respond_to do |format|
       format.html { redirect_to root_path }
       format.turbo_stream
